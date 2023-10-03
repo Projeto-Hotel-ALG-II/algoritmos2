fc3b47d2f7530adcdf848ee714369abb3d8042f2
Matheus Garcia Batista <bmatheusgarcia@gmail.com>
2023-10-02 23:04:50 -0300

    Att Quick Tools

--- quick_tools.h
+++ quick_tools.h
@@ -1,6 +1,22 @@
-#include <stdio.h>
-#include <windows.h>
+#ifndef quick_tools_h
+#define quick_tools_h
 
-void clear(){
+#include <stdio.h>
+#include <conio.h>
+#include <windows.h>
+/*
+ * Fun‡Æo limpa o prompt de comando 
+*/
+void clearPrompt(){
     system("cls");
 }
+
+/*
+ * Fun‡Æo pausa a aplica‡Æo e mostra o texto "Pressione qualquer tecla para continuar..." 
+*/
+void pausaSist(){
+    printf("\nPressione qualquer tecla para continuar . . .");
+    getch();
+}
+
+#endif
